40ccbbcff45de05c3dd1ccebb42153fd16166b66
Gwangho Jo <111489868+JGwanghou@users.noreply.github.com>
2024-03-18 20:47:54 +0900

    [Bronze II] Title: 음계, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,27 +1,31 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        int ascCount = 0;
+        int descCount = 0;
+        String result = "";
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] inputs = new int[8];
 
-        String result = "";
-        int ascendingCount = 0;
-        int descendingCount = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < inputs.length; i++) {
-            inputs[i] = sc.nextInt();
-            
-            if(inputs[i] == i + 1){ ascendingCount++; }
-            else if(inputs[i] == 8 - i){ descendingCount++; }
-        }
-        sc.close();
+            inputs[i] = Integer.parseInt(st.nextToken());
 
-        if(ascendingCount == 8){ result = "ascending"; }
-        else if(descendingCount == 8){ result = "descending"; }
-        else{ result = "mixed"; }
+            if(inputs[i] == i + 1) ascCount++;
+            if(inputs[i] == 8 - i) descCount++;
+        }
+
+        if(ascCount == 8) result = "ascending";
+        else if(descCount == 8) result = "descending";
+        else result = "mixed";
 
         System.out.println(result);
     }
