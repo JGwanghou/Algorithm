253db5d586c7e113fe63271ebf54c4a5233a943f
Gwangho Jo <111489868+JGwanghou@users.noreply.github.com>
2024-04-07 15:52:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15928 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[26];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        String input = br.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if(arr[ch - 'a'] == -1){
+                arr[ch - 'a'] = i;
+            }
+        }
+
+        for (int result : arr){
+            System.out.print(result + " ");
+        }
+    }
+}
