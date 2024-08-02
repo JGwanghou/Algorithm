5dbc0462fb56c7c7e4b7a662ce1c1213663fd1bb
Gwangho Jo <111489868+JGwanghou@users.noreply.github.com>
2024-08-02 10:38:52 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.59 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -4,14 +4,11 @@ class Solution {
         char[] chars = String.valueOf(n).toCharArray();
         Arrays.sort(chars);
 
-//        String temp = ""; 이거보다는 String빌더 사용 권장(이유=?)
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = chars.length - 1; i >= 0; i--) {
             stringBuilder.append(chars[i]);
         }
 
-        String result = String.valueOf(stringBuilder);
-        
-        return Long.parseLong(result);
+        return Long.parseLong(stringBuilder.toString());
     }
 }
