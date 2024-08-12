5ffab6963cd204ba94c59b06d570bfa39ba789e0
Gwangho Jo <111489868+JGwanghou@users.noreply.github.com>
2024-08-12 22:23:33 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,41 +1,19 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 배열 초기화
-        int[] student = new int[n+2];
+        int answer = n - lost.length;
         
-        // 도난 당한 학생의 경우 -1로 저장
-        for(int i = 0; i < lost.length; i++){
-            student[lost[i]]--;
-        }
-        
-        // 여분 체육복 있는 학생의 경우 1로 저장
         for(int i = 0; i < reserve.length; i++){
-            student[reserve[i]]++;
-        }
-        
-        // ex) [1, -1, 1, -1, 1] 
-        for(int i = 0; i < student.length; i++){
-            // 내가 현재 도난 당했다면
-            if(student[i] == -1){
-                // 내 앞의 사람이 여분이 있나 확인
-                if(student[i - 1] == 1){
-                    // 있다면 빌려주고 둘다 0으로 만들기
-                    student[i]++;
-                    student[i - 1]--;
+            if(i >= lost.length || answer == n){
+                break;
+            }
+            
+            for(int j = 0; j < lost.length; j++){
+                if(reserve[i] == lost[i] - 1 || reserve[i] == lost[i] + 1){
+                    answer++;
                 }
-                // 앞 사람이 없다면 뒷 사람이 여분이 있나 확인
-                else if(student[i + 1] == 1){
-                    // 있다면 빌려주고 둘다 0으로 만들기
-                    student[i]++;
-                    student[i + 1]--;
-                }
-                // 앞 뒤 둘다 여분이 없다면
-                else{
-                    n--;
-                } 
             }
         }
         
-        return n;
+        return answer;
     }
 }
