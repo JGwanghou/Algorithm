4020c24481cca4d4bd7c2eae6548bd181190f85a
Gwangho Jo <111489868+JGwanghou@users.noreply.github.com>
2025-01-18 23:07:40 +0900

    [level 2] Title: 올바른 괄호, Time: 24.87 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,27 +1,30 @@
-
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        if(s.charAt(0) == ')'){
+        if(s.charAt(0) == ')' || s.charAt(s.length() - 1) == '('){
             return false;
         }
-
+        
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (stack.isEmpty()) {
-                stack.push(c);
+        
+        for(char ch : s.toCharArray()){
+            
+            if(ch == '('){
+                stack.push(ch);
                 continue;
-            }
-
-            if(stack.peek() == '(' && c == ')'){
-                stack.pop();
             }else{
-                stack.push(c);
+                if(stack.isEmpty()){
+                    return false;
+                }
+                
+                if(stack.peek() == '('){
+                    stack.pop();
+                }
             }
-
+            
         }
-
+        
         return stack.isEmpty();
     }
 }
